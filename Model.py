221e401d2936f83782b8d11a0e45ee8ded172f60
MoriokaReimen <nakata.kei.job@gmail.com>
2020-11-14 13:55:31 +0900

    [update] update Model

--- Model.py
+++ Model.py
@@ -6,6 +6,7 @@ from dataclasses import asdict
 @dataclass
 class TableItem:
     id: int
+    active: bool
     title: str
     importance: int
     urgency: int
@@ -16,18 +17,18 @@ class Database():
     def __init__(self, db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS tasks ( id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE, importance INTEGER DEFAULT 2, urgency INTEGER DEFAULT 2, detail TEXT, memo TEXT);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS tasks ( id INTEGER PRIMARY KEY, active BOOLEAN NOT NULL, title TEXT NOT NULL UNIQUE, importance INTEGER DEFAULT 2, urgency INTEGER DEFAULT 2, detail TEXT, memo TEXT);")
 
     def fetch(self, query):
         rows = self.cur.execute(query).fetchall()
-        return [TableItem(row[0], row[1], row[2], row[3], row[4], row[5]) for row in rows ]
+        return [TableItem(row[0], row[1], row[2], row[3], row[4], row[5], row[6]) for row in rows ]
 
     def fetch_all(self):
         rows = self.cur.execute("SELECT * FROM tasks;").fetchall()
-        return [TableItem(row[0], row[1], row[2], row[3], row[4], row[5]) for row in rows ]
+        return [TableItem(row[0], row[1], row[2], row[3], row[4], row[5], row[6]) for row in rows ]
 
     def insert(self, item):
-        self.cur.execute("INSERT OR REPLACE INTO tasks VALUES (NULL, ?, ?, ?, ?, ?)", (item.title, item.importance, item.urgency, item.detail, item.memo))
+        self.cur.execute("INSERT OR REPLACE INTO tasks VALUES (NULL, ?, ?, ?, ?, ?, ?)", (item.active, item.title, item.importance, item.urgency, item.detail, item.memo))
         self.conn.commit()
 
     def remove(self, id):
@@ -45,13 +46,14 @@ class DailyGenerator():
     def generate(self, rows):
         text = str()
         for row in rows:
-            text += \
-                "Title:{title}\n" \
-                "====================================================================================================\n" \
-                "Importance:\t{importance}\n" \
-                "Urgency:\t{urgency}\n" \
-                "Detail:\n{detail}\n" \
-                .format(**asdict(row))
+            if row.active:
+                text += \
+                    "Title:{title}\n" \
+                    "====================================================================================================\n" \
+                    "Importance:\t{importance}\n" \
+                    "Urgency:\t{urgency}\n" \
+                    "Detail:\n{detail}\n" \
+                    .format(**asdict(row))
 
         print(text)
 
@@ -63,8 +65,8 @@ class DailyGenerator():
 if __name__ == '__main__':
     daily = DailyGenerator()
     db = Database("database.db")
-    db.insert(TableItem(0, "Jiro Ramen", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
-    db.insert(TableItem(0, "Jiro Ramen2", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
-    db.insert(TableItem(0, "Jiro Ramen3", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
+    db.insert(TableItem(0, True, "Jiro Ramen", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
+    db.insert(TableItem(0, False, "Jiro Ramen2", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
+    db.insert(TableItem(0, True, "Jiro Ramen3", 5, 5, "Eating Jiro Ramen with Hang", "Hang loves vegitables so much."))
     rows = db.fetch_all()
     daily.generate(rows)
