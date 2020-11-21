2a4dbe9dd5ca226c9d548b57009237e9e1b3cc03
MoriokaReimen <nakata.kei.job@gmail.com>
2020-11-21 13:58:08 +0900

    [update] add fts table

--- Model/Database.py
+++ Model/Database.py
@@ -21,6 +21,24 @@ class Database():
         self.cur = self.conn.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS tasks ( task_id INTEGER PRIMARY KEY, active BOOLEAN NOT NULL, title TEXT NOT NULL UNIQUE, importance INTEGER DEFAULT 2, urgency INTEGER DEFAULT 2, detail TEXT, memo TEXT, man_hour FLOAT, create_date TIMESTAMP);")
 
+        # check if tasks_fts table exists
+        table_found = self.cur.execute(
+            "SELECT count(name) FROM sqlite_master WHERE type='table' AND name='tasks_fts'").fetchall()[0]
+
+        # if the count is 1, then table exists
+        if not table_found:
+            # create and copy from tasks table
+            self.cur.execute("CREATE VIRTUAL TABLE IF NOT EXISTS tasks_fts USING fts4(task_id INTEGER PRIMARY KEY, active BOOLEAN NOT NULL, title TEXT NOT NULL UNIQUE, importance INTEGER DEFAULT 2, urgency INTEGER DEFAULT 2, detail TEXT, memo TEXT, man_hour FLOAT, create_date TIMESTAMP);")
+            self.cur.execute("INSERT INTO tasks_fts SELECT * FROM tasks;")
+
+        # Add trigers
+        self.cur.execute("CREATE TRIGGER IF NOT EXISTS on_task_add AFTER INSERT ON tasks BEGIN INSERT INTO tasks_fts (task_id, active, title, importance, urgency, detail, memo, man_hour, create_data) VALUES (new.task_id, new.active, new.title, new.importance, new.urgency, new.detail, new.memo, new.man_hour, new.create_data); END;")
+        self.cur.execute("CREATE TRIGGER IF NOT EXISTS on_task_delete AFTER DELETE ON tasks BEGIN INSERT INTO tasks_fts (task_id, active, title, importance, urgency, detail, memo, man_hour, create_data) VALUES ('delete', old.task_id, old.active, old.title, old.importance, old.urgency, old.detail, old.memo, old.man_hour, old.create_data); END;")
+        self.cur.execute("CREATE TRIGGER IF NOT EXISTS on_task_update AFTER UPDATE ON tasks BEGIN INSERT INTO tasks_fts (task_id, active, title, importance, urgency, detail, memo, man_hour, create_data) VALUES ('delete', old.task_id, old.active, old.title, old.importance, old.urgency, old.detail, old.memo, old.man_hour, old.create_data); INSERT INTO tasks_fts (task_id, active, title, importance, urgency, detail, memo, man_hour, create_data) VALUES (new.task_id, new.active, new.title, new.importance, new.urgency, new.detail, new.memo, new.man_hour, new.create_data); END;")
+
+        # updte db
+        self.conn.commit()
+
     def fetch(self, task_id):
         """Get task from database
 
